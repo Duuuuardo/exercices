3bef7ecfcdb07f3db20685c524cd3d78982b48c6
Duuuuardo <eduardofabisiak@proton.me>
2023-10-18 19:40:07 -0300

    :think: update: exercicios em C

---
+++ C/datatype.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int age = 10;
+  double grade = 9.5;
+  char school[] = "Complexo";
+  const int ingressation = 2010;
+  const char biologicalSex[] = "male";
+
+  return 0;
+}
